e4d92f7753ef5c3c6b23d8b316a2c15a7ae1014e
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2021-08-10 10:06:06 -0500

    unificando likes y dislkkes

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const templateCard = document.getElementById('template-card').content;
 const fragment = document.createDocumentFragment();
 let like = [];
 let disLike = [];
-//let control = 0;
+let control = 0;
 document.addEventListener('DOMContentLoaded', () => {
     loadData(data);
 })
@@ -24,51 +24,45 @@ const loadData = data => {
     items.appendChild(fragment);
 }
 
-//Carga evento Click
-items.addEventListener('click', e =>{
-    addTotal(e) // va a la funcion addTotal
+items.addEventListener('click', e => {
+    addTotal(e)
 })
-//funcion addTotal:
+
 const addTotal = e => {
-    if (e.target.classList.contains('btn-dark')){ //si toco el boton negro (like)
-    setLike(e.target.parentElement); //vaya a la funcion setLike
-    }else if(e.target.classList.contains('btn-danger')){ //contrario si toco (dislike)
-        setDislike(e.target.parentElement); //vaya a funcion setdisLike
-        }
+    if (e.target.classList.contains('btn-dark')) {
+        setTotal(e.target.parentElement);
+        control = 1;
+    } else if (e.target.classList.contains('btn-danger')) {
+        setTotal(e.target.parentElement);
+        control = 2;
+    }
 }
-//funcion seLike:
-const setLike = objeto =>{
-    const boton = { //creamos un objeto donde se almacene la cantidad y el id de la card
-    id: objeto.querySelector('.btn-dark').dataset.id,
-    cantidad : 1 //lo definimos en 1
+
+const setTotal = objeto => {
+    const botonLike = {
+        id: objeto.querySelector('.btn-dark').dataset.id,
+        cantidad: 1
     }
-    if(like.hasOwnProperty(boton.id)){ //si da click al boton
-        boton.cantidad = like[boton.id].cantidad+1; //sumele al objeto +1
-        objeto.querySelector('#like').textContent = boton.cantidad; //guarde en el objeto
-    }else{// de lo contrario para hacer que aumente el primer like en 1
-        like[boton.id] = {...boton}; 
-        objeto.querySelector('#like').textContent = boton.cantidad;
+    const botonDislike = {
+        id: objeto.querySelector('.btn-danger').dataset.id,
+        cantidad: 1
     }
 
-    like[boton.id] = {...boton}; 
-
-}
-//funcion sedisLike:
-const setDislike = objeto =>{
-    const boton = { //creamos un objeto donde se almacene la cantidad y el id de la card
-    id: objeto.querySelector('.btn-danger').dataset.id,
-    cantidad : 1//lo definimos en 1
-    }
-    if(disLike.hasOwnProperty(boton.id)){//si da click al boton
-        boton.cantidad = disLike[boton.id].cantidad+1;//sumele al objeto +1
-        objeto.querySelector('#disLike').textContent = boton.cantidad;//guarde en el objeto
-    }else{// de lo contrario para hacer que aumente el primer like en 1
-        disLike[boton.id] = {...boton};
-        objeto.querySelector('#disLike').textContent = boton.cantidad;
+    if (like.hasOwnProperty(botonLike.id) && control == 1) {
+        botonLike.cantidad = like[botonLike.id].cantidad + 1;
+        objeto.querySelector('#total-likes').textContent = botonLike.cantidad;
+    } else if (disLike.hasOwnProperty(botonDislike.id) && control == 2) {
+        botonLike.cantidad = like[botonLike.id].cantidad - 1;
+        objeto.querySelector('#total-likes').textContent = botonLike.cantidad;
+    }  else {
+        like[botonLike.id] = { ...botonLike };
+        disLike[botonDislike.id] = { ...botonDislike };
+        objeto.querySelector('#total-likes').textContent = botonLike.cantidad;
     }
 
-    disLike[boton.id] = {...boton};
-
+    like[botonLike.id] = { ...botonLike };
+    console.log(like[botonLike.id])
+    console.log(`control es ${control}`);
 }
 
 
